89f985549b38cb9b073a1c8213f6ba171662d183
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-09-25 01:04:00 +0200

    implementet echo server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::net::TcpListener;
+use tokio::io::{AsyncWriteExt, BufReader, AsyncBufReadExt};
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket, _addr) = listener.accept().await.unwrap();
+    let(reader, mut writer) = socket.split();
+    let mut reader =  BufReader::new(reader);
+    let mut line = String::new();
+
+    loop{
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0 {
+            break;
+        }
+        writer.write_all(&line.as_bytes()).await.unwrap();
+        line.clear();
+    }
 }
