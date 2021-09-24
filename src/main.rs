0ef595b2e0ebb5820731e64b5c66732dce30e0f3
AkiraTenchi <34791734+AkiraTenchi@users.noreply.github.com>
2021-09-25 01:27:15 +0200

    added chat functionality

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
-use tokio::io::{AsyncWriteExt, BufReader, AsyncBufReadExt};
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx, mut _rx) = broadcast::channel::<String>(10);
     loop {
         let (mut socket, _addr) = listener.accept().await.unwrap();
+
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
 
         tokio::spawn(async move {
             let (reader, mut writer) = socket.split();
@@ -14,12 +19,19 @@ async fn main() {
             let mut line = String::new();
 
             loop {
-                let bytes_read = reader.read_line(&mut line).await.unwrap();
-                if bytes_read == 0 {
-                    break;
+                tokio::select! {
+                   result =  reader.read_line(&mut line) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    }
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        writer.write_all(&msg.as_bytes()).await.unwrap();
+                    }
                 }
-                writer.write_all(&line.as_bytes()).await.unwrap();
-                line.clear();
             }
         });
     }
