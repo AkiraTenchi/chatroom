9df044025b26b70249f4433cf74782b69afc71a0
AkiraTenchi <jimjan2002@gmail.com>
2021-10-16 23:44:40 +0200

    added more advanced functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 use std::convert::Infallible;
 
+use futures::TryStreamExt as _;
 use hyper::server::Server;
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Method, Request, Response, StatusCode};
+
+enum EchoOpt {
+    None,
+    Uppercase,
+    Reverse,
+}
 
 #[tokio::main]
 async fn main() {
@@ -12,7 +19,7 @@ async fn main() {
     // create Service to enable connection
     let make_svc = make_service_fn(|_conn| async {
         // Converts function into 'Service'
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(req_manager))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
@@ -22,8 +29,43 @@ async fn main() {
     }
 }
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn req_manager(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    match (req.method(), req.uri().path()) {
+        (&Method::GET, "/") => Ok(Response::new("index".into())),
+        (&Method::GET, "/helloworld") => hello_world().await,
+        (&Method::POST, "/echo") => echo(req, EchoOpt::None).await,
+        (&Method::POST, "/echo/uppercase") => echo(req, EchoOpt::Uppercase).await,
+        (&Method::POST, "/echo/reverse") => echo(req, EchoOpt::Reverse).await,
+        _ => Ok(Response::builder()
+            .status(StatusCode::NOT_FOUND)
+            .body("Not found".into())
+            .unwrap()),
+    }
+}
+
+async fn hello_world() -> Result<Response<Body>, hyper::Error> {
     Ok(Response::new("Hello,World".into()))
 }
 
-// async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {}
+async fn echo(req: Request<Body>, modi: EchoOpt) -> Result<Response<Body>, hyper::Error> {
+    let mut response = Response::new(Body::empty());
+    match modi {
+        EchoOpt::None => *response.body_mut() = req.into_body(),
+        EchoOpt::Uppercase => {
+            let mapping = req.into_body().map_ok(|chunk| {
+                chunk
+                    .iter()
+                    .map(|byte| byte.to_ascii_uppercase())
+                    .collect::<Vec<u8>>()
+            });
+            *response.body_mut() = Body::wrap_stream(mapping);
+        }
+        EchoOpt::Reverse => {
+            let full_body = hyper::body::to_bytes(req.into_body()).await?;
+            let reversed = full_body.iter().rev().cloned().collect::<Vec<u8>>();
+            *response.body_mut() = reversed.into();
+        }
+    }
+
+    Ok(response)
+}
